14575346a08848ce0f5d143268dc487a90d80178
Geoff Bourne <itzgeoff@gmail.com>
2012-11-21 22:01:55 -0600

    * Starting traversal of list items

--- at-redbox.js
+++ at-redbox.js
@@ -1,4 +1,28 @@
 $("div#pagecontent").css("background-color", "green");
 
-$("th.title").after("<th class='availableAt'>Available at</th>");
-$("td.title").after("<td class='availableAt'>?</td>");
+var compactList = $("div.list.compact");
+
+if (compactList.length > 0) {
+	// Make room for our column
+	$("th.num_votes").remove();
+	$("td.num_votes").remove();
+	$("th.created").remove();
+	$("td.created").remove();
+	
+	$("th.title", compactList).after("<th class='availableAt'>At</th>");
+	$("td.title", compactList).after("<td class='availableAt'>?</td>");
+	
+	$("tr.list_item").each(function(index) {
+		// Only process feature films
+		if ($("td.title_type", this).html() == "Feature") {
+			$("td.title_type", this).css("color", "blue");
+			
+			// FOR DEVELOPMENT, only process one specific title
+			if ($("td.title > a", this).html() == "The Dark Knight Rises") {
+				$("td.title > a", this).css("color", "red");
+				
+				
+			}
+		}
+	});
+}
