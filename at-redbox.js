89a4f44123c3736c639b0e6a6cb9915ae53119d5
Geoff Bourne <itzgeoff@gmail.com>
2012-11-21 23:47:28 -0600

    * Successfully invokes Netflix titles query API

--- at-redbox.js
+++ at-redbox.js
@@ -18,11 +18,33 @@ if (compactList.length > 0) {
 			$("td.title_type", this).css("color", "blue");
 			
 			// FOR DEVELOPMENT, only process one specific title
-			if ($("td.title > a", this).html() == "The Dark Knight Rises") {
+			if ($("td.title > a", this).text() == "The Dark Knight Rises") {
 				$("td.title > a", this).css("color", "red");
 				
-				
+				var accessor = { 
+						consumerKey: "en66ns8syqhw7ukgsx3e94rx",
+						consumerSecret: "gR4JE8NmXh" };
+			    var message = { method: "GET",
+			    				action: "http://api-public.netflix.com/catalog/titles",
+			                    parameters: []
+			                  };
+			    
+			    message.parameters.push(["term", $("td.title > a", this).text()]);
+			    message.parameters.push(["start_index", "0"]);
+			    message.parameters.push(["max_results", "1"]);
+			    
+			    OAuth.completeRequest(message, accessor);
+			    
+			    var urlToCall = message.action + "?" + 
+			    OAuth.formEncode(message.parameters);
+			    
+			    console.log(urlToCall);
+			    
+			    $.get(urlToCall, function(data) {
+			    	console.log("Response is", data);
+			    }, "xml");
 			}
+			
 		}
 	});
 }
